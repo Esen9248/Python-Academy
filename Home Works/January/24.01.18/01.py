ed1939e9d5c8f565908a314432fd5c8e36c28fbc
Esen9248 <eee.coool@gmail.com>
2018-01-26 23:01:23 +0600

    26.01.18 commit

--- Home Works/January/24.01.18/01.py
+++ Home Works/January/24.01.18/01.py
@@ -2,13 +2,19 @@ from mod_01 import (
 	square_area,
 	circle_area, 
 	triangle_area,
-	square_equation,
-	cube_addition,
+	quadratic_equation,
+	cube_of_sum,
 )
 
+x_sq = int(input('Enter lengh of square side: '))
+x_cr = int(input('Enter circle radius: '))
+a_tr, h_tr = [int(i) for i in input('Enter lengh of triangle side and height: ').split()]
+a_qe, b_qe, c_qe = [int(i) for i in input('Enter a, b, c in ax^2+bx+c=0 addicion: ').split()]
+x_cs, y_cs = [int(i) for i in input('Enter a, b in (a+b)^3 addition: ').split()]
+
 if __name__ == '__main__':
-	print(square_area(4))
-	print(circle_area(3))
-	print(triangle_area(6,5))
-	print(square_equation(2,4,2)) # 2x^2 + 4x + 2 = 0
-	print(cube_addition(3,4))
+	print(square_area(x_sq))
+	print(circle_area(x_cr))
+	print(triangle_area(a_tr, h_tr))
+	print(quadratic_equation(a_qe,b_qe,c_qe)) # 2x^2 + 4x + 2 = 0
+	print(cube_of_sum(x_cs,y_cs))
--- Home Works/January/24.01.18/mod_01.py
+++ Home Works/January/24.01.18/mod_01.py
@@ -2,15 +2,15 @@ from math import sqrt
 Pi = 3.14
 
 def square_area(a):
-	return 'S=', a**2
+	return 'S(square)=', a**2
 
 def circle_area(r):
-	return 'S=', Pi*(r**2)
+	return 'S(circle)=', Pi*(r**2)
 
 def triangle_area(a,h):
-	return 'S=', (a/2)*h
+	return 'S(triangle)=', (a/2)*h
 
-def square_equation(a,b,c):
+def quadratic_equation(a,b,c):
 	d = (b**2)-(4*a*c)
 	if d < 0:
 		return 'Error'
@@ -22,6 +22,6 @@ def square_equation(a,b,c):
 		x2 = (-b - sqrt(d)) / (2*a)	
 		return 'x1=', x1, 'x2=', x2
 
-def cube_addition(a,b):
-	# (a+b)^2 =  a^3 + 3a^2b + 3ab^2 + b^3
-	return '(a+b)^2=', a**3 + (3*(a**2)*b) + (3*a*(b**2) + b**3
+def cube_of_sum(a,b):
+	# (a+b)^3 =  a^3 + 3a^2b + 3ab^2 + b^3
+	return '(a+b)^3=', a**3 + (3*(a**2)*b) + (3*a*(b**2)) + b**3
